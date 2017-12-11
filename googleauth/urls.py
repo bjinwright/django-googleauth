fc72c27230e0a7fa0d2bc25db3fd86ec9123c756
Brian Jinwright <bjinwright@qwigo.com>
2017-12-11 16:43:08 -0500

    Update urls.py

--- googleauth/urls.py
+++ googleauth/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import *
-from .googleauth.views import login, callback, logout
+from .views import login, callback, logout
 urlpatterns = [
     url(r'^login/$', login, name='googleauth_login'),
     url(r'^callback/$', callback, name='googleauth_callback'),
